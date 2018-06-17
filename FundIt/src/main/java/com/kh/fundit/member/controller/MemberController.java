52ea22d01830f97b86c8d9dc7b08d12be5d5ca7e
flyingboy122 <flyingboy122@naver.com>
2018-06-17 17:51:14 +0900

    메일보내기
    김태윤

--- FundIt/src/main/java/com/kh/fundit/member/controller/MemberController.java
+++ FundIt/src/main/java/com/kh/fundit/member/controller/MemberController.java
@@ -1,13 +1,27 @@
 package com.kh.fundit.member.controller;
 
+import javax.mail.internet.MimeMessage;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.core.io.FileSystemResource;
+
 @Controller
 public class MemberController {
-
+	
+	@Autowired
+	private JavaMailSenderImpl mailSender;
 	
 	@RequestMapping("/member/login.do")
 		public String memberLogin() {
@@ -30,9 +44,28 @@ public class MemberController {
 		return "member/findPwdEnd";
 	}
 	
+	
+	
 	@RequestMapping("/member/findPwdSendLink.do")
-	public void findPwdSendLink(@RequestParam String email) {
+	public void findPwdSendLink(@RequestParam final String email, HttpServletRequest request) {
 	     System.out.println("email = "+ email);
+	     
+	     final MimeMessagePreparator preparator = new MimeMessagePreparator() {
+	         @Override
+	         public void prepare(MimeMessage mimeMessage) throws Exception {
+	             final MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+	             helper.setFrom("flyingboy147@naver.com");
+	             helper.setTo(email);
+	             helper.setSubject("fundit에서 비밀번호 링크를 보내드립니다.");
+	             helper.setText("메일보내기 태스트");
+	         }
+	     };
+
+	     mailSender.send(preparator);
+
+	   
+	     
+	    
 	}
 	
 }
